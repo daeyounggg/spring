e9f3221158da4c01423d4f1b9169648369bda5f6
daeyounggg <kdyzab@gmail.com>
2023-09-04 21:53:01 +0900

    스프링부트

---
+++ 6.Spring_SpringBoot/spring_study/exam04/src/test/java/org/koreait/Exam04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // junit5, mockito, spring-test / MockMvc
+class Exam04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
