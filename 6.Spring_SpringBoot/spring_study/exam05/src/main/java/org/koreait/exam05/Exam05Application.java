e6c90122a17e8c0474f76ed9415e02224d5498ce
daeyounggg <kdyzab@gmail.com>
2023-09-05 20:47:56 +0900

    스프링부트

---
+++ 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/exam05/Exam05Application.java
@@ -0,0 +1,13 @@
+package org.koreait.exam05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exam05Application.class, args);
+	}
+
+}
---
+++ 6.Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/exam05/Exam05ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait.exam05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
