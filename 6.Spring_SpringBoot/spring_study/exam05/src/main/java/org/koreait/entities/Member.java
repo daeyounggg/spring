38bffd519f2b7c1a42448c52d3a3a1cc50cf0c26
daeyounggg <kdyzab@gmail.com>
2023-09-06 20:36:39 +0900

    스프링부트

--- 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/Member.java
+++ 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/Member.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.koreait.constants.Role;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity // 클래스를 엔티티로 선언 -> JPA에서 관리, 테이블에 대응하는 클래스
 @Table(name="es_member")
@@ -16,10 +17,17 @@ import java.time.LocalDateTime;
 public class Member {
     @Id @GeneratedValue
     private Long userNo;
+
+    @Column(name="memId", length=40, unique = true, nullable = false)
     private String userId;
+
+    @Column(length = 65, nullable = false)
     private String userPw;
+
+    @Column(length=40, nullable=false)
     private String userNm;
-    @Lob
+
+    @Transient
     private String introduction;
 
     @Enumerated(EnumType.STRING) // enum 함수
@@ -30,4 +38,10 @@ public class Member {
 
     @UpdateTimestamp
     private LocalDateTime modDt;
+
+    /**
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+    */
+
 }
