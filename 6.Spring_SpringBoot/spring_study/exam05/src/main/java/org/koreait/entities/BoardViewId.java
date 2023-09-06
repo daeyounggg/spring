4230d0451d1c0a66c497df16d3fefe4a84745614
daeyounggg <kdyzab@gmail.com>
2023-09-06 21:26:46 +0900

    스프링부트

---
+++ 6.Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/BoardViewId.java
@@ -0,0 +1,14 @@
+package org.koreait.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardViewId {
+    private Long id;
+    private int uid;
+
+}
