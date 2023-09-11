0cbcc2c32239583a215c1faa220de8ab8d1b7611
daeyounggg <kdyzab@gmail.com>
2023-09-11 20:19:13 +0900

    공부 자료

---
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
@@ -0,0 +1,30 @@
+package org.koreait.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Member extends BaseEntity{
+    @Id @GeneratedValue
+    private Long userNo;
+
+    @Column(length=40, unique = true, nullable = false)
+    private String userId;
+
+    @Column(length=65, nullable = false)
+    private String userPw;
+
+    @Column(length=40, nullable = false)
+    private String userNm;
+
+    @Column(length=100, nullable = false)
+    private String email;
+
+    @Column(length=11)
+    private String mobile;
+}
