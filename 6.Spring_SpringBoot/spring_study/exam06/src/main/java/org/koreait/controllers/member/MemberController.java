2242bffc7c96f9ef019685ec6c87e517dd75680b
daeyounggg <kdyzab@gmail.com>
2023-09-11 22:00:40 +0900

    공부 자료

---
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/controllers/member/MemberController.java
@@ -0,0 +1,36 @@
+package org.koreait.controllers.member;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/join")
+    public String join(@ModelAttribute JoinForm joinForm){
+
+        return "member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinPs(@Valid JoinForm joinForm, Errors errors){
+
+        if(errors.hasErrors()){
+            return "member/join";
+        }
+
+        return "redirect:/member/login";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "member/login";
+    }
+}
