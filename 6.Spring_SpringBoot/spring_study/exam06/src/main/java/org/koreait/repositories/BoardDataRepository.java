643f46c31a8372ad6799fa7db1705278ec1465e2
daeyounggg <kdyzab@gmail.com>
2023-09-13 21:38:15 +0900

    공부 자료

---
+++ 6.Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/repositories/BoardDataRepository.java
@@ -0,0 +1,8 @@
+package org.koreait.repositories;
+
+import org.koreait.entities.BoardData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
+
+}
